e0efb2c5d7d6a622e02877ecfe40217e9b83f90f
HyJan <hejian@52tt.com>
2020-04-29 19:11:29 +0800

    bug

--- src/main/java/com/example/jwtdemo/controller/UserController.java
+++ src/main/java/com/example/jwtdemo/controller/UserController.java
@@ -30,18 +30,19 @@ public class UserController {
     /**
      * 获取当前用户的信息
      * 从头部获取jwt 然后进行进行解析获取真正的信息
+     *
      * @param authorization
      * @param authentication
      * @param request
      * @return
      */
     @GetMapping("/userInfo")
-    public Object GetCurrentUser(@RequestHeader("Authorization")String authorization, Authentication authentication, HttpServletRequest request){
+    public Object GetCurrentUser(@RequestHeader("Authorization") String authorization, Authentication authentication, HttpServletRequest request) {
         String header = request.getHeader("Authorization");
         log.info("获取到的签证为 {}", header);
-        log.info("注解拿到的 {}", authentication);
+        log.info("注解拿到的 {}", authorization);
         // 使用工具类的工具进行字符串的截取
-        String token = StrUtil.subAfter(authorization,"bearer ",false);
+        String token = StrUtil.subAfter(authorization, "bearer ", false);
         return Jwts.parser()
                 // 设置签证秘钥
                 .setSigningKey(assignKey.getBytes(StandardCharsets.UTF_8))
